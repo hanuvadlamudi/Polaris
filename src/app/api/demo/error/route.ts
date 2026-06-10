b50fb3248eb017f7430e1d91eb99b686164d7bb6
hanuvadlamudi <hanuvadlamudivhr@gmail.com>
2026-06-10 15:03:45 +0530

    add outline of home page

--- src/app/api/demo/error/route.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-export async function POST() {
-    const error = new Error("Simulated Server-side API Failure");
-    Sentry.captureException(error);
-    throw error;
-}
